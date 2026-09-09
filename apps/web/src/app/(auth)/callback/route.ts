0ddbd6a348f9cfe7b8e66306e7650fa69f2ff437
Elijah Hausman <elijah.hausman@outlook.com>
2026-09-08 17:57:25 -0700

    Updated callback route to use process instead of the @/lib/env.ts helper

--- apps/web/src/app/(auth)/callback/route.ts
+++ apps/web/src/app/(auth)/callback/route.ts
@@ -1,8 +1,10 @@
 import { handleAuth } from "@workos-inc/authkit-nextjs";
-import { env } from "@/lib/env";
 import { outingsPath } from "@/lib/paths";
 
+// Uses process instead of the @/lib/env.ts helper because the non NEXT_PUBLIC_*
+// variables aren't available at build time and therefore cause the application to crash
+
 export const GET = handleAuth({
-	baseURL: env.NEXT_PUBLIC_APP_URL,
+	baseURL: process.env.NEXT_PUBLIC_APP_URL,
 	returnPathname: outingsPath(),
 });
