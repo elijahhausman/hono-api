8da5374dc97ad9c07d43054060a9e6584f59a90d
Elijah Hausman <elijah.hausman@outlook.com>
2026-09-10 05:27:07 -0700

    Removed path aliases

--- apps/api/src/routes/outings.ts
+++ apps/api/src/routes/outings.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 
 import { requireAuth } from "../middleware/auth.js";
-import { getOutings } from "@/services/outings.js";
+import { getOutings } from "../services/outings.js";
 
 const app = new Hono();
 
