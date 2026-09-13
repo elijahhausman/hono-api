fe6787fcb660751da2facef69921da2c985db055
Elijah Hausman <elijah.hausman@outlook.com>
2026-09-13 14:25:32 -0700

    Test confirmed v3? (experimental)

--- apps/api/src/data/env.ts
+++ apps/api/src/data/env.ts
@@ -1,13 +1,13 @@
-import { loadEnv } from "@repo/env";
-import { z } from "zod";
+// import { loadEnv } from "@repo/env";
+// import { z } from "zod";
 
-export const env = loadEnv(
-  z.object({
-    WORKOS_CLIENT_ID: z.string().min(1),
-    WORKOS_API_KEY: z.string().min(1),
-    WORKOS_TOKEN_AUDIENCE: z.string().min(1),
-    WORKOS_TOKEN_ISSUER: z.string().min(1),
-    NEXT_PUBLIC_APP_URL: z.string().url(),
-    PORT: z.coerce.number().default(3001),
-  }),
-);
+// export const env = loadEnv(
+//   z.object({
+//     WORKOS_CLIENT_ID: z.string().min(1),
+//     WORKOS_API_KEY: z.string().min(1),
+//     WORKOS_TOKEN_AUDIENCE: z.string().min(1),
+//     WORKOS_TOKEN_ISSUER: z.string().min(1),
+//     NEXT_PUBLIC_APP_URL: z.string().url(),
+//     PORT: z.coerce.number().default(3001),
+//   }),
+// );
