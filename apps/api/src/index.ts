e8c614b3817095bf8aae56b3dcbe54ab567ccc58
Elijah Hausman <elijah.hausman@outlook.com>
2026-09-13 14:29:58 -0700

    Railway is broken? (experimental)

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,6 +4,12 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
+console.log(process.env.WORKOS_CLIENT_ID)
+console.log(process.env.WORKOS_API_KEY)
+console.log(process.env.WORKOS_TOKEN_AUDIENCE)
+console.log(process.env.WORKOS_TOKEN_ISSUER)
+console.log(process.env.DATABASE_URL)
+
 // import { env } from "./data/env.ts";
 import outingRoutes from "./routes/outings.ts";
 
@@ -16,7 +22,7 @@ app.route("/outings", outingRoutes);
 serve(
   {
     fetch: app.fetch,
-    port: Number(process.env.PORT),
+    port: Number(process.env.PORT) || 8080,
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
