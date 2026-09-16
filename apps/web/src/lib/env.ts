1a1610d1f4ab76a220ffb5714801f1b4aa91c634
Elijah Hausman <elijah.hausman@outlook.com>
2026-09-16 11:48:28 -0700

    Updated shadcn components and web app @/src/lib/env.ts file

--- apps/web/src/lib/env.ts
+++ apps/web/src/lib/env.ts
@@ -6,8 +6,8 @@ export const env = loadEnv(
 		WORKOS_CLIENT_ID: z.string().min(1),
 		WORKOS_API_KEY: z.string().min(1),
 		WORKOS_COOKIE_PASSWORD: z.string().min(1),
-		NEXT_PUBLIC_WORKOS_REDIRECT_URI: z.string().min(1),
-		NEXT_PUBLIC_APP_URL: z.string().min(1),
-		NEXT_PUBLIC_API_URL: z.string().min(1),
+		NEXT_PUBLIC_WORKOS_REDIRECT_URI: z.url(),
+		NEXT_PUBLIC_APP_URL: z.url(),
+		NEXT_PUBLIC_API_URL: z.url(),
 	}),
 );
